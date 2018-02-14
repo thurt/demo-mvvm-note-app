f9bb40219037497d86f57d51470796e12b52f573
thurt <taylor.a.hurt@gmail.com>
2018-02-14 17:57:00 +0000

    removes api.Error because it is no longer used (errors are handled by parent context)

--- src/api.ts
+++ src/api.ts
@@ -5,23 +5,6 @@ export const basePath = '/api';
 export const posts = new api.PostsApi(undefined, basePath);
 export const auth = new api.AuthApi(undefined, basePath);
 
-export type UnaryError = {
-  error: string;
-  code: number;
-};
-
-export type StreamError = {
-  error: {
-    grpc_code: number;
-    http_code: number;
-    message: string;
-    http_status: string;
-  };
-};
-
-// Error is the interface returned by the api in the response body when a request error has occurred. Common examples of request errors that would cause the server to respond with an Error would be when the the request contains invalid or missing values, or when a request is made for a non-existant entity.
-export type Error = UnaryError | StreamError;
-
 type chunk = {
   done: boolean;
   value: any;
